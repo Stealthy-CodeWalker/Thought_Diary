94f02b5959073b44be2240c85163fff548794065
Stealthy-CodeWalker <webdev.vanguard@gmail.com>
2023-04-05 09:12:12 +0530

    cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,26 @@
 import './App.css'
 import React from 'react'
 import Nav from './components/Navbar'
-// import Home from './pages/home/Home'
-// import About from './pages/about/about'
-// import Blog from './pages/blogs/blog'
-// import { BrowserRouter, Routes, Route, Switch, Link } from 'react-router-dom'
+import Home from './pages/home/Home'
+import About from './pages/about/about'
+import Blog from './pages/blogs/blog'
+import Diary from './pages/thought_diary/Index'
+import { BrowserRouter, Routes, Route} from 'react-router-dom'
+
 
 function App() {
 
   return (
-    <Nav/>
+    <>
+      <BrowserRouter>
+      <Nav/>
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/about" element={<About/>} />
+          <Route path="/blog" element={<Blog/>} />              <Route path="/diary" element={<Diary/>}/> 
+        </Routes>
+      </BrowserRouter>
+    </>
   )
 }
 
